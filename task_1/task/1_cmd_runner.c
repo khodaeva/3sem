0bd584ef1901cd2c9f9dffdd50c8c8dbfe5f800b
khodaeva <liana199923@yandex.ru>
2017-09-23 04:59:13 +0300

    the 1st attempt

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -9,3 +9,48 @@
    a) if execvp is used, string splitting into "path" and "args" is all you need.
 3. Collect exit codes via waitpid/WEXITSTATUS.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+	char c = 'a';
+	int argc, i, status;
+
+	while (tolower(c) != 'n') {
+		const pid_t pid = fork();
+		if (pid == -1) {
+			printf("fork error\n");
+			return(-1);
+		}
+		if (pid != 0) {
+			waitpid(pid, &status, 0);
+			printf("Ret code: %d\nOnce again?(y/n)\n", WEXITSTATUS(status));
+			scanf("%c", &c);
+		} else {
+			printf("argc = ?\n");
+			scanf("%d", &argc);
+			char ** arg;
+			arg = (char **) malloc ((argc + 1) * sizeof(char *));
+			for (i = 0; i < argc; i++) {
+				arg[i] = (char*) malloc (100 * sizeof(char));
+			}
+			for (i = 0; i < argc; i++) {
+				scanf("%s", arg[i]);
+			}
+			arg[argc] = NULL;
+			execvp(arg[0], arg);
+			for (i = 0; i < (argc + 1); i++) {
+				free(arg[i]);
+			}
+
+			free(arg);
+		}
+	}
+	return 0;
+}
